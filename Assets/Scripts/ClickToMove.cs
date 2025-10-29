ac240ae3d473cb60e40b9becf0a82e6af10cab27
Ercilian <davidamunoz99@gmail.com>
2025-10-29 12:47:52 +0100

    clicktomove

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -3,26 +3,26 @@ using NUnit.Framework.Constraints;
 using UnityEngine.InputSystem;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ClickToMove : MonoBehaviour
 
 {
-    
-    [SerializeField] private float moveSpeed;
     Vector3 destination;
     Rigidbody rb;
-    [SerializeField] Transform destinoDummnie;
+    [SerializeField] Transform destinationDummy;
+    private NavMeshAgent agent;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.destination = destinationDummy.position;
     }
 
    
     void Update()
     {
-        destination = destinoDummnie.position;
-
         if (Mouse.current != null && Mouse.current.rightButton.wasPressedThisFrame)
         {
             HandleClick();
@@ -30,25 +30,13 @@ public class ClickToMove : MonoBehaviour
     }
     private void HandleClick()
     {
-        // Lanzar un Raycast desde la cámara al punto clicado
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
         RaycastHit hit;
-        // Suponiendo que el suelo tiene el layer "Ground" o puedes usar cualquier layer
-        if (Physics.Raycast(ray, out hit))
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out hit, 100f))
         {
-            // Mover el destinoDummnie al punto de impacto
-            destinoDummnie.position = hit.point;
-            destination = hit.point;
-            StartCoroutine(MoveToPosition(destination));
+            destinationDummy.position = hit.point;
+            agent.destination = destinationDummy.position;
         }
-    }
 
-    IEnumerator MoveToPosition(Vector3 _destination)
-    {
-    Vector3 moveDirection = _destination - transform.position;
-    moveDirection = moveDirection.normalized;
-    rb.linearVelocity = Vector3.zero; // Reinicia la velocidad antes de aplicar la nueva fuerza
-    rb.AddForce(moveDirection * moveSpeed, ForceMode.VelocityChange);
-    yield return null;
     }
 }
