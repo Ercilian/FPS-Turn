6de60a4fc419203e17c568b2bcac0d99dc067fc6
Ercilian <davidamunoz99@gmail.com>
2025-12-05 20:43:35 +0100

    HealthBar

--- Assets/Scripts/Core/Character.cs
+++ Assets/Scripts/Core/Character.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
@@ -9,22 +10,48 @@ public class Character : MonoBehaviour
     [SerializeField] float cur_HP;
     [SerializeField] float max_HP;
     [SerializeField] protected float defByEquipment;
-    public Animator Animator;
-
     protected int level = 1;
-    private bool isDead = false;
+    private bool isDead = false;   
+    public Image HealthBar;
+    [SerializeField] GameObject healthBarPrefab;
+    private RectTransform playerHUDRect;
+    private GameObject playerHUDInstance;
+    
+    public Animator Animator;
+    private Canvas canvas;
 
     protected virtual void Start()
     {
         characterName = gameObject.name;
         cur_HP = max_HP;
+
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        playerHUDInstance = Instantiate(healthBarPrefab, canvas.transform);
+        playerHUDRect = playerHUDInstance.GetComponent<RectTransform>();
+        HealthBar = playerHUDInstance.transform.Find("HP/HP Bar").GetComponent<Image>();
+        AssignBars(HealthBar);
     }
 
+    public void AssignBars(Image healthBar)
+    {
+        HealthBar = healthBar;
+        UpdateHealthBar();
+    }
+
+    public void UpdateHealthBar()
+    {
+        if (HealthBar != null)
+        {
+            Debug.Log("Updating health bar for " + characterName + ": " + cur_HP + "/" + max_HP);
+            HealthBar.fillAmount = cur_HP / max_HP;
+        }
+    }
     public void TakeDamage(float Dmg)
     {
         Animator.SetTrigger("Hit");
         float finalDmg = Dmg - defByEquipment;
         cur_HP -= finalDmg;
+        UpdateHealthBar();
         Debug.Log(characterName + " took " + finalDmg + " damage. Current HP: " + cur_HP + "/" + max_HP);
         IsAlive();
     }
@@ -36,6 +63,7 @@ public class Character : MonoBehaviour
         {
             cur_HP = max_HP;
         }
+        UpdateHealthBar();
         Debug.Log(characterName + " healed " + healAmount + " HP. Current HP: " + cur_HP + "/" + max_HP);
     }
     
@@ -59,12 +87,22 @@ public class Character : MonoBehaviour
         {
             TurnManager.Instance.RemoveUnit(units);
         }
+        if (playerHUDInstance != null)
+        {
+            Destroy(playerHUDInstance);
+        }
         yield return new WaitForSeconds(5f);
         Destroy(gameObject);
     }
 
+    void LateUpdate()
+    {
+        if (playerHUDRect != null && canvas != null)
+        {
+            Vector3 worldPosition = transform.position + Vector3.up * 5f; // Ajusta la altura
+            Vector3 screenPosition = Camera.main.WorldToScreenPoint(worldPosition);
 
-
-
-
+            playerHUDRect.position = screenPosition;
+        }
+    }
 }
